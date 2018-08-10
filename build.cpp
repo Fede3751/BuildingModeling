3d3d5ca5192adc9d6772320ee4854905886de230
Fede3751 <ftrombetti@hotmail.it>
2018-08-11 01:00:33 +0200

    First version upload

---
+++ build.cpp
@@ -0,0 +1,461 @@
+/*
+
+BUILDING MODELLING.
+Computer Graphic Project 2017-2018
+Student: Federico Trombetti
+ID: 1665803
+
+See .h for documentation
+
+
+*/
+
+
+#include "../yocto/yocto_gl.h"
+using namespace std::literals;
+#include "build.h"
+
+
+//instantiate Direction only once and use it as a constant value
+auto Dir = Direction{};
+
+
+ModResult createResult(std::vector<int> faces, Building* b) {
+
+	ModResult res;
+
+
+	for (int i : faces) {
+
+		if (b->faces[i].orientation == Dir.UP)
+			res.up = i;
+		else if (b->faces[i].orientation == Dir.DOWN)
+			res.down = i;
+		else if (b->faces[i].orientation == Dir.NORTH)
+			res.north = i;
+		else if (b->faces[i].orientation == Dir.SOUTH)
+			res.south = i;
+		else if (b->faces[i].orientation == Dir.EAST)
+			res.east = i;
+		else if (b->faces[i].orientation == Dir.WEST)
+			res.west = i;
+	}
+
+	return res;
+
+}
+
+
+ModResult createBuilding(Building* b, float w, float h, float d) {
+
+
+
+	//allocate space for first faces (6 in total) and points (8 in total)
+	b->points = std::vector<ygl::vec3f>(8);
+	b->faces = std::vector<Face>(6);
+
+	auto points = b->points;
+	auto faces = b->faces;
+
+
+	//create first points
+	b->points[0] = { 0, 0, 0 };
+	b->points[1] = { w, 0, 0 };
+	b->points[2] = { w, d, 0 };
+	b->points[3] = { 0, d, 0 };
+
+	b->points[4] = { 0, 0, h };
+	b->points[5] = { w, 0, h };
+	b->points[6] = { w, d, h };
+	b->points[7] = { 0, d, h };
+
+	b->faces[0] = { { 1,0,3,2 }, Dir.DOWN };
+	b->faces[1] = { { 4,5,6,7 }, Dir.UP };
+	b->faces[2] = { { 0,1,5,4 }, Dir.SOUTH };
+	b->faces[3] = { { 3,0,4,7 }, Dir.WEST };
+	b->faces[4] = { { 1,2,6,5 }, Dir.EAST };
+	b->faces[5] = { { 2,3,7,6 }, Dir.NORTH };
+
+
+	return createResult({ 0,1,2,3,4,5 }, b);
+
+};
+
+
+std::vector<int> subdivideFace(Building* b, int n) {
+
+
+	Face f = b->faces[n];
+	auto v = f.v;
+	int s = b->points.size();
+	int i = b->faces.size();
+
+
+
+	//generate new points
+	ygl::vec3f c = (b->points[v.x] + b->points[v.y] + b->points[v.z] + b->points[v.w]) / 4;		//center
+	ygl::vec3f xy = (b->points[v.x] + b->points[v.y]) / 2;										// x-y
+	ygl::vec3f yz = (b->points[v.y] + b->points[v.z]) / 2;										// y-z
+	ygl::vec3f zw = (b->points[v.z] + b->points[v.w]) / 2;										// z-w
+	ygl::vec3f wx = (b->points[v.w] + b->points[v.x]) / 2;										// w-x
+
+
+	b->points.push_back(c);			//s
+	b->points.push_back(xy);		//s+1
+	b->points.push_back(yz);		//s+2
+	b->points.push_back(zw);		//s+3
+	b->points.push_back(wx);		//s+4
+
+	b->faces[n].v = { s + 4,s,s + 3,v.w };
+	b->faces.push_back({ { s,s + 2,v.z,s + 3 }, f.orientation });
+	b->faces.push_back({ { v.x,s + 1,s,s + 4 }, f.orientation });
+	b->faces.push_back({ { s + 1, v.y, s + 2, s }, f.orientation });
+
+
+	return std::vector<int>{n, i, i + 1, i + 2};
+
+};
+
+
+std::vector<int> subdivideFace(Building* b, int f, int w, int h) {
+
+
+	auto v = b->faces[f].v;
+
+
+	auto wAmnt = (b->points[v.y] - b->points[v.x]) / w;
+	auto hAmnt = (b->points[v.x] - b->points[v.w]) / h;
+
+
+	//actual size of the grid (input is in term of faces, so points between faces are more)
+	int pH = h + 1;
+	int pW = w + 1;
+
+	//allocate space for grid of points (so that they are easily found when creating faces)
+	std::vector<int> grid = std::vector<int>(pH*pW);
+
+	//save the existing edges in the right position in the grid
+	grid[0] = v.w;
+	grid[pW - 1] = v.z;
+	grid[pW*pH - 1] = v.y;
+	grid[pW*(pH - 1)] = v.x;
+
+
+	int s = b->points.size();
+	for (int i = 0; i < pW; i++) {
+		for (int j = 0; j < pH; j++) {
+
+			//do not replicate existing points
+			if ((i == 0 || i == pW - 1) && (j == 0 || j == pH - 1))
+				continue;
+
+			grid[j*pW + i] = s++;
+			b->points.push_back(b->points[v.w] + wAmnt*i + hAmnt * j);
+		}
+	}
+
+
+	b->faces[f].v = { grid[pW], grid[pW + 1], grid[1], grid[0] };
+
+	std::vector<int> res = std::vector<int>(w*h);
+	res[0] = f;
+
+	int fs = b->faces.size();
+	for (int i = 0; i < w; i++) {
+		for (int j = 0; j < h; j++) {
+
+			//skip first tile (as it is generated from the previous face being resized)
+			if (j == 0 && i == 0)
+				continue;
+
+			res[j*w + i] = fs++;
+			b->faces.push_back({ { grid[(j + 1)*pW + i], grid[(j + 1)*pW + i + 1], grid[j*pW + i + 1], grid[j*pW + i] }, b->faces[f].orientation });
+
+		}
+	}
+
+	return res;
+
+
+};
+
+
+std::vector<std::vector<int>> subdivideFace(Building* b, std::vector<int> f, int w, int h, int from, int to) {
+
+	if (from == 0 && to == 0)
+		to = f.size();
+
+	std::vector<std::vector<int>> res = std::vector<std::vector<int>>(to - from);
+
+	for (int i = 0; i < to - from; i++)
+		res[i] = subdivideFace(b, f[i], w, h);
+
+
+	return res;
+
+
+}
+
+
+//void Building::subdivideFace(int n, int f) {};
+
+
+
+ModResult pullFace(Building* b, int f, float value) {
+
+
+	int s = b->points.size();
+	int i = b->faces.size();
+
+	auto verse = value / std::abs(value);
+
+	printf("%f\n", verse);
+
+	std::vector<int> temp = std::vector<int>{ f, i, i + 1, i + 2, i + 3 };
+
+	//duplicate points
+	for (auto v : b->faces[f].v)
+		b->points.push_back(b->points[v]);
+
+	//link the face which has to be pushed to the new points and save the old points to create the faces
+	//(also save the new ones on a shorter variable for easier use)
+	ygl::vec4i oldP = b->faces[f].v;
+	b->faces[f].v = { s, s + 1, s + 2, s + 3 };
+	ygl::vec4i newP = b->faces[f].v;
+
+
+	//create new faces
+	ygl::vec3f middlePoint =
+		(b->points[oldP.x] + b->points[oldP.y] + b->points[oldP.z] + b->points[oldP.w] +
+			b->points[newP.x] + b->points[newP.y] + b->points[newP.z] + b->points[newP.w]) / 4
+		;
+
+
+	if (b->faces[f].orientation == Dir.SOUTH) {
+		b->faces.push_back({ { oldP.x, newP.x, newP.w, oldP.w }, Dir.WEST * verse });
+		b->faces.push_back({ { newP.y, newP.x, oldP.x, oldP.y }, Dir.DOWN * verse });
+		b->faces.push_back({ { newP.y, oldP.y, oldP.z, newP.z }, Dir.EAST * verse });
+		b->faces.push_back({ { newP.w, newP.z, oldP.z, oldP.w }, Dir.UP   * verse });
+	}
+	else if (b->faces[f].orientation == Dir.NORTH) {
+		b->faces.push_back({ { newP.y, oldP.y, oldP.z, newP.z }, Dir.WEST * verse });
+		b->faces.push_back({ { oldP.x, oldP.y, newP.y, newP.x }, Dir.DOWN * verse });
+		b->faces.push_back({ { oldP.x, newP.x, newP.w, oldP.w }, Dir.EAST * verse });
+		b->faces.push_back({ { oldP.z, oldP.w, newP.w, newP.z }, Dir.UP });
+	}
+	else if (b->faces[f].orientation == Dir.EAST) {
+		b->faces.push_back({ { newP.y, oldP.y, oldP.z, newP.z }, Dir.NORTH * verse });
+		b->faces.push_back({ { newP.x, oldP.x, oldP.y, newP.y }, Dir.DOWN  * verse });
+		b->faces.push_back({ { oldP.x, newP.x, newP.w, oldP.w }, Dir.SOUTH * verse });
+		b->faces.push_back({ { oldP.w, newP.w, newP.z, oldP.z }, Dir.UP    * verse });
+	}
+	else if (b->faces[f].orientation == Dir.WEST) {
+		b->faces.push_back({ { oldP.x, newP.x, newP.w, oldP.w }, Dir.NORTH * verse });
+		b->faces.push_back({ { oldP.y, newP.y, newP.x, oldP.x }, Dir.DOWN  * verse });
+		b->faces.push_back({ { newP.y, oldP.y, oldP.z, newP.z }, Dir.SOUTH * verse });
+		b->faces.push_back({ { newP.z, oldP.z, oldP.w, newP.w }, Dir.UP    * verse });
+	}
+	else if (b->faces[f].orientation == Dir.UP) {
+		b->faces.push_back({ { oldP.w, oldP.x, newP.x, newP.w }, Dir.WEST  * verse });
+		b->faces.push_back({ { oldP.x, oldP.y, newP.y, newP.x }, Dir.SOUTH * verse });
+		b->faces.push_back({ { oldP.y, oldP.z, newP.z, newP.y }, Dir.EAST  * verse });
+		b->faces.push_back({ { oldP.z, oldP.w, newP.w, newP.z }, Dir.NORTH * verse });
+	}
+	else if (b->faces[f].orientation == Dir.DOWN) {
+		b->faces.push_back({ { newP.z, newP.y, oldP.y, oldP.z }, Dir.WEST  * verse });
+		b->faces.push_back({ { newP.y, newP.x, oldP.x, oldP.y }, Dir.SOUTH * verse });
+		b->faces.push_back({ { newP.x, newP.w, oldP.w, oldP.x }, Dir.EAST  * verse });
+		b->faces.push_back({ { newP.w, newP.z, oldP.z, oldP.w }, Dir.NORTH * verse });
+	}
+
+
+	//pull face
+	for (auto v : b->faces[f].v) {
+		b->points[v] += b->faces[f].orientation * value;
+	}
+
+
+	//link new faces
+	return createResult(temp, b);
+
+};
+
+std::vector<ModResult> pullFace(Building* b, std::vector<int> f, float value, int from, int to) {
+
+	
+
+	if (from == 0 && to == 0)
+		to = f.size();
+
+	std::vector<ModResult> res = std::vector<ModResult>(to - from);
+
+	for (int i = 0; i < to - from; i++)
+		res[i] = pullFace(b, f[i + from], value);
+
+
+	return res;
+
+
+}
+
+
+std::vector<int> subFace(Building* b, int f, float xSub, float ySub, float offsetX, float offsetY) {
+
+
+	auto v = b->faces[f].v;
+
+	auto wOuter = (b->points[v.y] - b->points[v.x]);
+	auto hOuter = (b->points[v.x] - b->points[v.w]);
+
+	auto wInner = wOuter * xSub;
+	auto hInner = hOuter * ySub;
+
+
+	auto grid = subdivideFace(b, f, 3, 3);
+
+	b->points[b->faces[grid[0]].v.z] = b->points[v.w] + (wOuter - wInner) / 2;
+	b->points[b->faces[grid[0]].v.x] = b->points[v.w] + (hOuter - hInner) / 2;
+	b->points[b->faces[grid[0]].v.y] = b->points[v.w] + (hOuter - hInner) / 2 + (wOuter - wInner) / 2;
+
+	b->points[b->faces[grid[2]].v.w] = b->points[v.z] - (wOuter - wInner) / 2;
+	b->points[b->faces[grid[2]].v.y] = b->points[v.z] + (hOuter - hInner) / 2;
+	b->points[b->faces[grid[2]].v.x] = b->points[v.z] + (hOuter - hInner) / 2  - (wOuter - wInner) / 2;
+
+	b->points[b->faces[grid[6]].v.y] = b->points[v.x] + (wOuter - wInner) / 2;
+	b->points[b->faces[grid[6]].v.w] = b->points[v.x] - (hOuter - hInner) / 2;
+	b->points[b->faces[grid[6]].v.z] = b->points[v.x] - (hOuter - hInner) / 2 + (wOuter - wInner) / 2;
+
+
+	b->points[b->faces[grid[8]].v.x] = b->points[v.y] - (wOuter - wInner) / 2;
+	b->points[b->faces[grid[8]].v.z] = b->points[v.y] - (hOuter - hInner) / 2;
+	b->points[b->faces[grid[8]].v.w] = b->points[v.y] - (hOuter - hInner) / 2 - (wOuter - wInner) / 2;
+
+
+
+	return grid;
+
+
+
+
+
+	
+}
+
+std::vector<int> subFace(Building* b, std::vector<int> f, float xSub, float ySub, int from, int to, float offsetX, float offsetY) {
+	
+	if (from == 0 && to == 0)
+		to = f.size();
+
+	std::vector<int> res = std::vector<int>(to - from);
+
+	for (int i = 0; i < to - from; i++)
+		res[i] = subFace(b, f[i], xSub, ySub, offsetX, offsetY)[4];
+
+
+	return res;
+}
+
+
+void makeRoofs(Building* b, ygl::vec3f axis) {
+
+	int i = 0;
+	for (Face f : b->faces) {
+		if (f.orientation == Dir.UP) {
+			pointFace(b, i, axis);
+		}
+		i++;
+	}
+
+}
+
+
+void pointFace(Building* b, int f, ygl::vec3f axis) {
+
+
+	auto v = b->faces[f].v;
+	int s = b->points.size();
+
+	ygl::vec3f p1, p2;
+
+	if (axis == Dir.EAST || axis == Dir.WEST) {
+		p1 = (b->points[v.x] + b->points[v.w]) / 2;  //s
+		p2 = (b->points[v.y] + b->points[v.z]) / 2;  //s+1
+
+		b->faces[f].v = { v.x, v.y, s + 1, s };
+		b->faces.push_back({ { s, s + 1, v.z, v.w }, Dir.UP });
+		b->faces.push_back({ { v.w, v.x, s, s }, Dir.EAST });
+		b->faces.push_back({ { v.y, v.z, s + 1, s + 1 }, Dir.WEST });
+
+	}
+	else if (axis == Dir.NORTH || axis == Dir.SOUTH) {
+		p1 = (b->points[v.x] + b->points[v.y]) / 2;
+		p2 = (b->points[v.z] + b->points[v.w]) / 2;
+
+		b->faces[f].v = { v.x, s, s + 1, v.w };
+		b->faces.push_back({ { s + 1, s, v.y, v.z }, Dir.UP });
+
+	}
+
+	b->points.push_back(p1);
+	b->points.push_back(p2);
+
+
+	b->points[s] += Dir.UP * 5;
+	b->points[s + 1] += Dir.UP * 5;
+
+
+};
+
+
+ygl::shape* buildingToShape(Building* b, ygl::material* mat) {
+
+
+
+	ygl::shape* res = new ygl::shape{};
+
+	res->mat = mat;
+	res->pos = b->points;
+	res->quads = std::vector<ygl::vec4i>();
+	res->texcoord = std::vector<ygl::vec2f>();
+	res->quads_texcoord = std::vector<ygl::vec4i>();
+	res->texcoord1 = std::vector<ygl::vec2f>();
+	res->quads_pos = std::vector<ygl::vec4i>();
+
+	for (auto f : b->faces) {
+		res->quads.push_back(f.v);
+	}
+	for (int i = 0; i < b->faces.size(); i++) {
+		res->texcoord.push_back(ygl::vec2f{ 100,100 });
+		res->texcoord1.push_back(ygl::vec2f{ 100,100 });
+
+		//res->quads_pos.push_back({ 4,5,6,7 });
+		//res->quads_texcoord.push_back({ 4,5,6,7 })
+	}
+
+	return res;
+
+};
+
+
+ygl::instance* buildingToInstance(Building* b, ygl::material* mat) {
+
+	ygl::instance* res = new ygl::instance{};
+
+	res->shp = new ygl::shape_group{};
+	res->shp->shapes = std::vector<ygl::shape *>(1);
+
+	res->shp->shapes[0] = buildingToShape(b, mat);
+
+	return res;
+
+}
+
+
+void addBuildingToScene(Building* b, ygl::scene* scn, ygl::material* mat) {
+
+	auto inst = buildingToInstance(b, mat);
+
+	scn->instances.push_back(inst);
+	scn->shapes.push_back(inst->shp);
+
+}
---
+++ build.h
@@ -0,0 +1,304 @@
+/*
+
+BUILDING MODELLING.
+Computer Graphic Project 2017-2018
+Student: Federico Trombetti
+ID: 1665803
+
+
+The aim of this library is to provide an easier access to model building through
+functions which make common transformations used to model building easier.
+
+Cardinal directions (plus up and down) are used to orientate the programmer while modelling
+the building through the functions.
+
+The heart of this library resides in a structure called ModResult, which is what
+most transformation functions will return. This structure lets the programmer access, in 
+a simpler way, the new faces which the transformation created.
+
+Function which subdivide faces without actually creating new shapes will instead return a vector
+of integers, which represent the indices of the new faces creaeted in a row-major order, starting
+from the top left of the face (with top it is meant the highest face in case of faces which span in height,
+or the face wich is north-wise most far, for those faces wich span from south to north).
+
+There are three type of functions:
+
+	-Transformation functions:
+			they modify the Building and return the proper result to continue working on the Building.
+
+	-Finalization funcionts: 
+			they modify the Building and then return nothing. Further modifications after these
+			functions have to keep in mind that may produce weird visual mistakes if applied improperly.
+			Some faces of a finalized Building cannot be modified correctly with the library itself.
+
+	-Conversion functions:
+			they are used to integrate the library with the Yocto one and will transform the Building
+			to the desired Yocto structure.
+
+
+
+
+
+Some conventions shared among the functions of the library:
+	
+	-All the functions take as first argument the Building to work with and as a second argument
+	the face to modify, every other argument usually depends on the function.
+
+	-Some functions which take in input a single face may have a generalization with a vector of
+	faces (i.e. pullFace), they basically iterate the desired operation on every given face, check
+	all the different overloads of the function to see which suits better your workflow.
+	Note that the return value of these generalization function will have a different return value
+	of the normal ones.
+
+
+	
+	
+Examples are provided for a better understanding on how to use the library, feel free to check them.
+
+
+*/
+
+#include "../yocto/yocto_gl.h"
+using namespace std::literals;
+
+
+//pre-declare structures
+struct Direction;
+struct Building;
+struct ModResult;
+struct Face;
+
+
+//----struct used for coordinate orientation, instantiated only once and----
+//----                    used as a constant value                      ----
+struct Direction {
+	ygl::vec3f UP = { 0,0,1 };
+	ygl::vec3f DOWN = { 0,0,-1 };
+	ygl::vec3f NORTH = { 0,1,0 };
+	ygl::vec3f SOUTH = { 0,-1,0 };
+	ygl::vec3f EAST = { 1,0,0 };
+	ygl::vec3f WEST = { -1,0,0 };
+};
+//--------------------------------------------------------------------------
+
+
+//struture used to orientate result values after a trasformation
+//whenever a transformation wich result in a new shape getting created is used,
+//a ModResult is returned, where every value of it cointains the indices of the
+//the faces of the new shape in the array of faces of the building.
+struct ModResult {
+	int up = 0;
+	int down = 0;
+	int north = 0;
+	int south = 0;
+	int east = 0;
+	int west = 0;
+};
+
+struct Face {
+	ygl::vec4i v;
+	ygl::vec3f orientation;
+};
+
+
+//structure used to moodel buildings, very similar to the shape in the ygl library,
+//but with some minor differences on variables for easier use.
+//It is additionally free from all those values which are not directly related to the
+//modelling of the building itself (i.e. textures)
+struct Building {
+	std::vector<Face> faces;
+	std::vector<ygl::vec3f> points;
+};
+
+/* 
+  Function used to create a ModResult from a transformation, it takes in input a
+  vector of integers and a Building, and returns a ModResult with the values of the
+  faces correctly assigned based on the indices and the Bulding given.
+  INPUT:
+		-faces	: the vector of faces to assign in the ModResult
+		-b		: the Buiiding which those faces belongs to
+  RETURN VALUE:
+		The ModResult of the given faces
+*/
+ModResult createResult(std::vector<int> faces, Building* b);
+
+/*
+  Function used to create the basic shape of a building (which is a parallelepiped
+  with the given size), it takes in input a Building b and three integers. The Building
+  b values are correctly set to create the structure and the ModResult with the indices
+  of the faces is returned.
+  INPUT:
+		-b		: the pointer to the building where the result wants to be saved
+		-w,h,d	: the size values of the building
+  RETURN VALUE:
+		The ModResult of the new structure created.
+
+*/
+ModResult createBuilding(Building* b, float w, float h, float d);
+
+/*
+  Function used to divide a given face in four smaller faces. It takes
+  in input a Building b and an integer n and divides the face n in
+  four faces.
+  INPUT:
+		-b: the building to modify
+		-n: the index of the face to subdivide
+  RETURN VALUE:
+		The vector with the indices of the new faces, sorted in
+		row-major order starting from the top left of the face.
+*/
+std::vector<int> subdivideFace(Building* b, int n);
+
+
+/*
+  Function used to divide a given face in w*h smaller faces. It takes
+  in input a Building b and three integers f,n,w and divides the face f in
+  w*h faces.
+  INPUT:
+		-b: the building to modify
+		-f: the index of the face to subdivide
+		-w,h: the number of new faces to create (w*h)
+  RETURN VALUE:
+		The vector with the indices of the new faces, sorted in
+		row-major order starting from the top left of the face.
+*/
+std::vector<int> subdivideFace(Building* b, int f, int w, int h);
+
+
+
+/*
+  Function used to subdivide in 3x3 smaller faces with the central one with
+  the size relative to the full face, multiplied by the xSub and ySub for every dimension.
+  This function is usually used to create the shape of a door or a windows by using it
+  in conjuction with pullFace and a negative value.
+  xSub and ySub must be strictly smaller than one, otherwise visual errors may appear.
+  INPUT:
+		-b: the Building to modify
+		-f: the face to subdivide
+		-xSub, ySub: the sizes of the central face relative to the original face
+					(they must be a positive number smaller than one)
+  RETURN VALUE:
+		The vector of indices of the faces. The central one will be the one in position
+		4. The others are returned in case they want to be further modified.
+*/
+std::vector<int> subFace(Building* b, int f, float xSub, float ySub, float offsetX=0, float offsetY=0);
+
+
+/*
+	Generalization function. Apply subFace to every face in f. Use the values "from"
+	and "to" to apply the function only to a slice of the complete vector
+	RETURN VALUE:
+			The vector of the faces in the middle obtained is returned.
+			NOTE: The single function returns every face created, not just the central one.
+*/
+std::vector<int> subFace(Building* b, std::vector<int> f, float xSub, float ySub, int from=0, int to=0, float offsetX=0, float offsetY=0);
+
+
+//void Building::subdivideFace(int n, int f) {};
+
+
+/*
+  Function used to pull out a block starting from a face.
+  It takes in input a Building b an integer f and a float value and
+  modify the Building by pulling the face f of a value amount.
+  The ModResult of the transformation is returned.
+  INPUT:
+		-b: the building to modify
+		-f: the index of the face to pull
+		-value: how much the face has to be pulled
+		(NOTE: the face can also be pushed by giving a negative value, but
+		some visual error may appear if you are pushing an entire face inside,
+		as connceted faces will overlap exactly on the same position. Push can be 
+		done without imperfections if the face doesn't have perpendicular 
+		connected faces to the starting one)
+  RETURN VALUE:
+		The ModResult of the transformation.
+*/
+ModResult pullFace(Building *b, int f, float value);
+
+
+/*
+	Generalization function. Apply pullFace to every face in f. Use the values "from"
+	and "to" to apply the function only to a slice of the complete vector
+	RETURN VALUE:
+			The vector of the different ModResult(s) is returned.
+
+*/
+std::vector<ModResult> pullFace(Building* b, std::vector<int> f, float value, int from = 0, int to = 0);
+
+
+
+/*
+  Function used to apply pointFace to all the faces pointing up.
+  NOTE: The use of this function is discouraged, not all faces which point up should be
+  a roof.
+  INPUT:
+		-b: the building to modify
+		-axis: the axis that the roofs top will follow when created
+  RETURN VALUE:
+		None.
+
+*/
+void makeRoofs(Building* b, ygl::vec3f axis);
+
+
+/*
+  Function used to make a face wich points up a roof.
+  Note that this function is final and won't return anything to work on,
+  the roofs are a shape which is not exactly aligned to any cardinal direction
+  and cannot be modified anymore by using the library, operations using the
+  normals can still be applied (as the vectors of the faces are all sorted in
+  clock-wise order).
+  You can still apply the same function to point up other faces, as the indices
+  of the old faces won't be touched.
+  INPUT:
+		-b: the building to modify
+		-f: the index of face to transform into a roof
+		-axis: the axis that the roofs top will follow when created
+  RETURN VALUE:
+		None.
+
+*/
+void pointFace(Building* b, int f, ygl::vec3f axis);
+
+
+/*
+  Function used to transform a building to a ygl::shape.
+  It takes in input a Building b and a material mat and creates a shape
+  with that material.
+  INPUT:
+		-b: the building to transform
+		-mat: the material to apply to the building
+  RETURN VALUE:
+		The ygl::shape of the building.
+
+*/
+ygl::shape* buildingToShape(Building* b, ygl::material* mat);
+
+
+/*
+  Function used to transform a building to a ygl::instance.
+  It takes in input a Building b and a material mat and creates an instance
+  with that material.
+  INPUT:
+		-b: the building to transform
+		-mat: the material to apply to the building
+  RETURN VALUE:
+		The ygl::instance of the building.
+
+*/
+ygl::instance* buildingToInstance(Building* b, ygl::material* mat);
+
+
+/*
+  Function used to add a Building to given scene. It takes in input a
+  Building b, a material mat and a scence scn and add the Building to the 
+  scene with the given material applied.
+  INPUT:
+		-b: the Building to add to the scene
+		-scn: the scene to add the Building to
+		-mat: the material to apply to the Building.
+  RETURN VALUE:
+		None.
+*/
+void addBuildingToScene(Building* b, ygl::scene* scn, ygl::material* mat);
